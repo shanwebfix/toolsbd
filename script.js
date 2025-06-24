ff501b803bf1598060ce277e7356e89be1c9a3a4
shanwebfix <shanwebfix@gmail.com>
2025-06-25 02:01:14 +0600

    update

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+ // io
+  document.addEventListener("contextmenu", (e) => e.preventDefault());
+  
+  document.addEventListener("keydown", function (e) {
+    if (
+      e.key === "F12" ||
+      (e.ctrlKey && e.shiftKey && e.key === "I") ||
+      (e.ctrlKey && e.key === "U")
+    ) {
+      e.preventDefault();
+    }
+  });
 
 
 // 📦 ইউটিলিটি ফাংশন: ইংরেজি সংখ্যা → বাংলা সংখ্যা
